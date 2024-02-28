1a4032dc172f34f6f68de4730120e602f8a41151
nasif <nasif@example.com>
2024-02-28 22:04:14 +0530

    final file

---
+++ script.js
@@ -0,0 +1,30 @@
+let item1=document.querySelector('.item1');
+let link1=document.querySelector('.link1');
+
+item1.addEventListener('click',function(){
+    link1.classList.toggle('active');
+})
+let item2=document.querySelector('.item2');
+let link2=document.querySelector('.link2');
+
+item2.addEventListener('click',function(){
+    link2.classList.toggle('active');
+})
+let item3=document.querySelector('.item3');
+let link3=document.querySelector('.link3');
+
+item3.addEventListener('click',function(){
+    link3.classList.toggle('active');
+})
+let item4=document.querySelector('.item4');
+let link4=document.querySelector('.link4');
+
+item4.addEventListener('click',function(){
+    link4.classList.toggle('active');
+})
+let item5=document.querySelector('.item5');
+let link5=document.querySelector('.link5');
+
+item5.addEventListener('click',function(){
+    link5.classList.toggle('active');
+})
